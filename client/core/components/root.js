2f3e5f53aad0d5d9cf1f59d267a9dc3d44caea9b
Pradel Léo <pradel.leo@gmail.com>
2016-10-10 15:23:40 +0200

    Fix lint

--- client/core/components/root.js
+++ client/core/components/root.js
@@ -1,33 +1,23 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import CircularProgress from 'material-ui/CircularProgress';
 import Header from './header';
 
-class Root extends Component {
-  render() {
-    const {
-      loading,
-      error, // eslint-disable-line
-      user,
-    } = this.props;
-    // TODO error handling
-    return (
-      <MuiThemeProvider>
-        <div>
-          <Header user={user} loading={loading} />
-          {loading ? <CircularProgress />
-            : <div className="container">
-              Hey
-            </div>}
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
+const Root = ({ loading, user }) =>
+  (<MuiThemeProvider>
+    <div>
+      <Header user={user} loading={loading} />
+      {loading ? <CircularProgress />
+        : <div className="container">
+          Hey
+        </div>}
+    </div>
+  </MuiThemeProvider>);
 
 Root.propTypes = {
   loading: PropTypes.bool,
-  error: PropTypes.string,
+  error: PropTypes.string, // eslint-disable-line
+  // TODO error handling
   user: PropTypes.object,
 };
 
