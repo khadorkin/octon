b1011ea97ef4c19db517c48c655f0a8781a6bc5a
Pradel Léo <pradel.leo@gmail.com>
2016-10-12 23:08:14 +0200

    Logout button

--- client/core/components/header.js
+++ client/core/components/header.js
@@ -1,7 +1,14 @@
 import React, { PropTypes } from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-const Header = ({ user, loading }) =>
-  (<nav className="navbar navbar-default navbar-fixed-top">
+const Header = ({ user, loading }) => {
+  const handleLogout = () => {
+    window.location.href = '/logout';
+  };
+  return (<nav className="navbar navbar-default navbar-fixed-top">
     <div className="container">
       <div className="navbar-header">
         <a className="navbar-brand" href="/">
@@ -12,9 +19,17 @@ const Header = ({ user, loading }) =>
         {loading ? <li><a>Loading ...</a></li> :
           <li className="navbar-avatar" style={{ backgroundImage: `url(${user.photo})` }} />
         }
+        <IconMenu
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem primaryText="Sign out" onTouchTap={handleLogout} />
+        </IconMenu>
       </ul>
     </div>
   </nav>);
+};
 
 Header.propTypes = {
   loading: PropTypes.bool,
