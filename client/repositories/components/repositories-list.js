f77429814e60a63227e2c7d298f6ec896962d7e3
Pradel Léo <pradel.leo@gmail.com>
2016-10-12 23:04:28 +0200

    FetchMore repositories

--- client/repositories/components/repositories-list.js
+++ client/repositories/components/repositories-list.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import TimeAgo from 'timeago-react';
 import CircularProgress from 'material-ui/CircularProgress';
 import Toggle from 'material-ui/Toggle';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const RepositoriesListItem = ({ repository }) =>
   (<div className="repositories-list-content">
     {repository.latestRelease ?
       <span className="release">Release {repository.latestRelease.tagName}</span>
       : <span className="release">No release</span>}
-    <span className="date"><TimeAgo datetime={new Date(repository.latestRelease.publishedAt)} /></span>
+    {repository.latestRelease ?
+      <span className="date"><TimeAgo datetime={new Date(repository.latestRelease.publishedAt)} /></span>
+      : null}
     <div className="media">
       <div className="media-left">
         <a href={repository.htmlUrl} target="_blank" rel="noopener noreferrer">
@@ -22,7 +25,7 @@ const RepositoriesListItem = ({ repository }) =>
           </a>
         </p>
         <div className="pull-right">
-          <Toggle label="Notifications" />
+          <Toggle label="Notifications" toggled={repository.starred} />
         </div>
       </div>
     </div>
@@ -33,28 +36,54 @@ RepositoriesListItem.propTypes = {
   repository: React.PropTypes.object,
 };
 
-const RepositoriesList = ({ loading, error, repositories }) =>
-  (<div>
-    {!loading && !error && repositories.length === 0 ?
-      <div className="center">
-        <p>You don&apos;t have starred repositories</p>
-      </div> : null}
-    <div className="repositories-list">
-      {repositories.map(repository =>
-        <RepositoriesListItem
-          key={repository.id}
-          repository={repository}
-        />
-      )}
-    </div>
-    {loading ? <CircularProgress /> : null}
-    {error ? <p className="bg-danger">{error.message}</p> : null}
-  </div>);
+class RepositoriesList extends Component {
+  constructor(props) {
+    super(props);
+    this.showMore = this.showMore.bind(this);
+    this.state = {
+      page: 1,
+    };
+  }
+
+  showMore() {
+    const page = this.state.page + 1;
+    this.setState({ page });
+    this.props.loadMoreRepositories({ page });
+  }
+
+  render() {
+    const { loading, error, repositories } = this.props;
+    const { page } = this.state;
+    return (<div>
+      {!loading && !error && repositories.length === 0 ?
+        <div className="center">
+          <p>You don&apos;t have starred repositories</p>
+        </div> : null}
+      <div className="repositories-list">
+        {repositories.map(repository =>
+          <RepositoriesListItem
+            key={repository.id}
+            repository={repository}
+          />
+        )}
+      </div>
+      {!loading && repositories.length * page === 100 * page ?
+        <div className="center">
+          <RaisedButton label="Show more" onTouchTap={this.showMore} />
+        </div>
+        : null}
+      {loading ? <CircularProgress /> : null}
+      {error ? <p className="bg-danger">{error.message}</p> : null}
+    </div>);
+  }
+}
+
 
 RepositoriesList.propTypes = {
-  loading: React.PropTypes.bool,
-  error: React.PropTypes.string,
-  repositories: React.PropTypes.array,
+  loading: PropTypes.bool,
+  error: PropTypes.string,
+  repositories: PropTypes.array,
+  loadMoreRepositories: PropTypes.func,
 };
 
 RepositoriesList.defaultProps = {
