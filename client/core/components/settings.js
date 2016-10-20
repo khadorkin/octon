92cfda2f2c4b74b3e139fa2e19f7a569647b7362
Pradel Léo <pradel.leo@gmail.com>
2016-10-20 14:54:05 +0200

    Refactor settings

--- client/core/components/settings.js
+++ client/core/components/settings.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Text from 'material-ui/Text';
 import IconButton from 'material-ui/IconButton';
 import {
@@ -9,45 +9,63 @@ import {
 } from 'material-ui/List';
 import Switch from 'material-ui/Switch';
 
-const Settings = ({ open, onSettings, user, setNotification }) => {
-  if (!open) return null;
-  const handleSetNotificationDaily = () => {
-    setNotification('daily', !user.dailyNotification, user);
-    // TODO error
-  };
-  const handleSetNotificationWeekly = () => {
-    setNotification('weekly', !user.weeklyNotification, user);
-    // TODO error
-  };
-  return (<div>
-    <Text type="title">
-      Settings
-    </Text>
-    <div className="pull-right">
-      <IconButton onClick={onSettings}>close</IconButton>
-    </div>
-    <List>
-      <ListItem>
-        <ListItemText primary="Daily notifications" />
-        <ListItemSecondaryAction>
-          <Switch checked={user.dailyNotification} onClick={handleSetNotificationDaily} />
-        </ListItemSecondaryAction>
-      </ListItem>
-      <ListItem>
-        <ListItemText primary="Weekly notifications" />
-        <ListItemSecondaryAction>
-          <Switch checked={user.weeklyNotification} onClick={handleSetNotificationWeekly} />
-        </ListItemSecondaryAction>
-      </ListItem>
-    </List>
-  </div>);
-};
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+    };
+  }
+
+  handleSetNotificationDaily = () => {
+    const { setNotification, user } = this.props;
+    this.setState({ error: '' });
+    setNotification('daily', !user.dailyNotification, user)
+      .catch(err => this.setState({ error: err.message }));
+  }
+
+  handleSetNotificationWeekly = () => {
+    const { setNotification, user } = this.props;
+    this.setState({ error: '' });
+    setNotification('weekly', '!user.weeklyNotification', user)
+      .catch(err => this.setState({ error: err.message }));
+  }
+
+  render() {
+    const { open, onSettings, user } = this.props;
+    const { error } = this.state;
+    if (!open) return null;
+    return (<div className="settings">
+      <Text type="title" className="title">
+        Settings
+        <div className="pull-right">
+          <IconButton onClick={onSettings}>close</IconButton>
+        </div>
+      </Text>
+      {error ? <p className="bg-danger">{error}</p> : null}
+      <List>
+        <ListItem>
+          <ListItemText primary="Daily notifications" />
+          <ListItemSecondaryAction>
+            <Switch checked={user.dailyNotification} onClick={this.handleSetNotificationDaily} />
+          </ListItemSecondaryAction>
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Weekly notifications" />
+          <ListItemSecondaryAction>
+            <Switch checked={user.weeklyNotification} onClick={this.handleSetNotificationWeekly} />
+          </ListItemSecondaryAction>
+        </ListItem>
+      </List>
+    </div>);
+  }
+}
 
 Settings.propTypes = {
-  open: PropTypes.bool,
-  user: PropTypes.object,
-  onSettings: PropTypes.func,
-  setNotification: PropTypes.func,
+  open: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  onSettings: PropTypes.func.isRequired,
+  setNotification: PropTypes.func.isRequired,
 };
 
 export default Settings;
