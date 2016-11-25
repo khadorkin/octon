06db859f8883bd84f26e1c440b2971196f3f1372
Pradel Léo <pradel.leo@gmail.com>
2016-11-25 14:54:59 +0100

    404 page client

---
+++ client/core/components/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () =>
+  (<div className="not-found">
+    <h3>404 page not found</h3><br />
+    <p>We are sorry but the page you are looking for does not exist.</p>
+  </div>);
+
+export default NotFound;
--- client/core/components/router.js
+++ client/core/components/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Root from '../containers/root';
 import Home from '../containers/home';
+import NotFound from './not-found';
 
 const RouterApp = () => (
   <Router history={browserHistory}>
@@ -9,6 +10,7 @@ const RouterApp = () => (
       <IndexRoute component={Home} />
       <Route path="repositories/:repositoryId" component={Home} />
       <Route path="settings" component={Home} />
+      <Route path="*" component={NotFound} />
     </Route>
   </Router>
 );
